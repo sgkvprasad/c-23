3dc0dbe07933ec6f36777bb90dfdfbcc7c10c074
svss aditya <107859335+sgkvprasad@users.noreply.github.com>
2022-08-08 17:51:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+var ground
+var world
+var ball
+var engine
+
+//Create namespace for Engine
+const Engine = Matter.Engine;
+//Create namespace for World
+const World = Matter.World;
+//Create namespace for Bodies
+const Bodies = Matter.Bodies;
+//Create namespace for Body
+const Body = Matter.Body;
+
+function setup() {
+  createCanvas(400,400);
+//create the 
+  engine = Engine.create();
+   
+  //Add world to the engine
+  world = engine.world;
+
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+  
+//create a ground
+ ground = Bodies.rectangle(200,390,400,20,ground_options);
+ 
+//add to world
+World.add(world,ground)
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background("black");
+  Engine.update(engine);
+  
+  
+
+  ellipse(ball.position.x,ball.position.y,20);
+  //write a rectangle function to display ground.
+ 
+   rect(ground.position.x,ground.position.y)
+
+  
+  
+}
+
